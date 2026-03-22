67325fd1067e06b6f422018929100985471262d0
adeebalamcoder-collab <adeeb.alam.coder@gmail.com>
2026-03-23 02:12:13 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "Bombay Fashion Beauty Zone",
+  "short_name": "BFBZ",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#f8f5f2",
+  "theme_color": "#f8f5f2",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icon.svg",
+      "sizes": "512x512",
+      "type": "image/svg+xml"
+    }
+  ]
+}
